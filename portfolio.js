7ffc3b5b2c4a4b2708aed835e4aff567f4b20527
curb4387 <teenawtran@gmail.com>
2025-10-13 13:40:36 -0400

    Change function name to be shorter

--- portfolio.js
+++ portfolio.js
@@ -5,10 +5,10 @@
 let currentTime = document.getElementById("time");
 let currentDate = document.getElementById("date");
 
-change_time_date(); // run immediately first
-setInterval(change_time_date, 1000);
+change_date(); // run immediately first
+setInterval(change_date, 1000);
 
-function change_time_date() {
+function change_date() {
     const time_date = new Date();
     currentTime.textContent = time_date.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
     currentDate.textContent = time_date.toLocaleDateString();
