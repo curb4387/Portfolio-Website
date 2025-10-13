39ba7111be79cf4ec3656bf62a5490141dcb0a7c
curb4387 <teenawtran@gmail.com>
2025-10-12 21:50:09 -0400

    Create JS file for main website page

---
+++ portfolio.js
@@ -0,0 +1,8 @@
+/* JavaScript file for main website page */
+"use strict";
+
+// Set time and date
+const time_date = new Date();
+
+
+document.getElementById("time").textContent = time_date.toLocaleString();
