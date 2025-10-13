b6ac7be9d7f3d78b868aa133a45112f11cecf46f
curb4387 <teenawtran@gmail.com>
2025-10-13 13:39:26 -0400

    Fix setting time and date

--- portfolio.js
+++ portfolio.js
@@ -1,8 +1,15 @@
 /* JavaScript file for main website page */
 "use strict";
 
-// Set time and date
-const time_date = new Date();
+// Set time and date in real time
+let currentTime = document.getElementById("time");
+let currentDate = document.getElementById("date");
 
+change_time_date(); // run immediately first
+setInterval(change_time_date, 1000);
 
-document.getElementById("time").textContent = time_date.toLocaleString();
+function change_time_date() {
+    const time_date = new Date();
+    currentTime.textContent = time_date.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+    currentDate.textContent = time_date.toLocaleDateString();
+}
