fee26a2a6c54de717d08971346a549f7bf0e5604
curb4387 <teenawtran@gmail.com>
2025-10-13 14:25:09 -0400

    Remove code to set time and date

--- portfolio.js
+++ portfolio.js
@@ -1,15 +1,4 @@
 /* JavaScript file for main website page */
 "use strict";
 
-// Set time and date in real time
-let currentTime = document.getElementById("time");
-let currentDate = document.getElementById("date");
-
-change_date(); // run immediately first
-setInterval(change_date, 1000);
-
-function change_date() {
-    const time_date = new Date();
-    currentTime.textContent = time_date.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
-    currentDate.textContent = time_date.toLocaleDateString();
-}
+// Popup window
